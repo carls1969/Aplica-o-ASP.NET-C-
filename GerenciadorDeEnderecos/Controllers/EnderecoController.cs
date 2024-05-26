09deb17098bb31e34f75c76be93ef65d41f81e0c
carls1969 <daniel282713@gmail.com>
2024-05-26 13:00:20 -0300

    Le os enderecos ja adicionados

---
+++ GerenciadorDeEnderecos/Controllers/EnderecoController.cs
@@ -0,0 +1,20 @@
+﻿using GerenciadorDeEnderecos.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GerenciadorDeEnderecos.Controllers;
+
+[ApiController]
+[Route("[Controller]")]
+public class EnderecoController : ControllerBase
+{
+    private static List<Endereco> enderecos = new List<Endereco>();
+
+    [HttpPost]
+    public void AdicionaEndereco ([FromBody] Endereco endereco)
+    {
+
+         enderecos.Add(endereco);
+         Console.WriteLine(endereco.Cep);
+        
+    }
+}
